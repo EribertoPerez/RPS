c01b2bdf57daa1b0c8caeecc88bc0967292d293b
ILoveLucyy <bobby.jose.perez@gmail.com>
2022-10-11 20:30:23 -0700

    hello world log

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 // 2. inputs (computerInput) and (userInput)
 // 3. Calculate ending value (win or loss)
 // 4. Display win or loss value
+// 5. Loop game (steps 1-4) for 5 rounds
+// 6. Determine a final winner
+
+console.log(`hello world!`)
+
